1b44c52125d9eabc120081613a4ccafda9b4509e
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-22 15:02:35 -0700

    TruncateWarningV1.js
    An archive of the first version of the truncate warning JavaScript script.

---
+++ GitHubby/Firefox/Truncation/OldVersions/TruncateWarningV1.js
@@ -0,0 +1,18 @@
+// Start of script
+/* TruncateWarning.js
+* This script warns the user when an upload may result in the current directory being truncated down to 1000 items. 
+* I have had issues with this in the past, and I feel like there should at least be warning before it deletes files
+*/
+// Start
+function truncateWarning {
+  // I don't know how to do this yet
+  console.log("Failed to warn of truncation");
+  alert("Failed to warn of truncation");
+}
+// End
+/* File info
+* File version: 1 (Monday, June 22nd 2020 at 3:02 pm)
+* File type: JavaScript (*.js)
+* Line count: 19
+*/
+// End of script
