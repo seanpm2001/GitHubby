8038a7828000a2de3125e23f9c71febf5e6b4add
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-22 14:53:55 -0700

    UploadLimitsV1.js
    An archive of the first version of the Upload Limits JavaScript file.

---
+++ GitHubby/Firefox/Limits/OldVersions/UploadLimitsV1.js
@@ -0,0 +1,24 @@
+// Start of script
+/* UploadLimits.js
+* This script sets limits for the file upload process on GitHub, as GitHub sets some limits for file uploads
+*/
+// Start
+// 100 file limit
+function fileLimit100 {
+  // I don't know how to access the file explorer via JavaSript right now
+  console.log("Failed to set 100 file quantity limit");
+  alert("Failed to set 100 file quantity limit");
+}
+// 25 Megabyte limit
+function fileLimit25MB {
+  // I don't know how to access the file explorer via JavaScript right now
+  console.log("Failed to set 25 Megabyte file limit");
+  alert("Failed to set 25 Megabyte file limit");
+}
+// End
+/* File info
+* File version: 1 (Monday, June 22nd 2020 at 2:53 pm)
+* File type: JavaScript file (*.js)
+* Line count (including blank lines): 25
+*/
+// End of script
